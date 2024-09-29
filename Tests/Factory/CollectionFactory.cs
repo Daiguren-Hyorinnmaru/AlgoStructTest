e641d44fb49b08e59aaca8de8e8fba4aaadf134b
tenzo_zangecu <tenzozangecu@gmail.com>
2024-09-30 01:30:08 +0300

    Create DataFactory & CollectionFactory

---
+++ Tests/Factory/CollectionFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Factory
+{
+    public enum CollectionType
+    {
+        Array,
+        List
+    }
+
+    public class CollectionFactory
+    {
+        DataFactory dataFactory;
+
+        public CollectionFactory(DataFactory dataFactory)
+        {
+            this.dataFactory = dataFactory;
+        }
+
+        public IList<T> GetListCollection<T>(DataType dataType, CollectionType collectionType, int length) where T : IComparable<T>
+        {
+            IList<T> CreateList(IEnumerable<T> items) => new List<T>(items);
+
+            IList<T> CreateArray(IEnumerable<T> items) => items.ToArray();
+
+            switch (collectionType)
+            {
+                case (CollectionType.Array):
+                    return CreateArray(GenerateValues<T>(dataType, length));
+                case (CollectionType.List):
+                    return CreateList(GenerateValues<T>(dataType, length));
+            }
+            return null;
+        }
+
+        private IEnumerable<T> GenerateValues<T>(DataType dataType, int length) where T : IComparable<T>
+        {
+            for (int i = 0; i < length; i++)
+            {
+                yield return (T)dataFactory.RandomValueType(dataType);
+            }
+        }
+    }
+}
---
+++ Tests/Factory/DataFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Sorts;
+
+namespace Tests.Factory
+{
+    public enum DataType
+    {
+        Integer,
+        String,
+        Double
+    }
+
+    public class DataFactory
+    {
+        public object RandomValueType(DataType dataType)
+        {
+            Random random = new Random();
+
+            switch (dataType)
+            {
+                case DataType.Integer:
+                    return random.Next();
+                case DataType.String:
+                    int length = random.Next(5, 15);
+                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                    return new string(Enumerable.Repeat(chars, length)
+                                                .Select(s => s[random.Next(s.Length)]).ToArray());
+                case DataType.Double:
+                    return random.NextDouble() * random.Next(1, 100);
+            }
+
+            return null;
+        }
+
+    }
+}
